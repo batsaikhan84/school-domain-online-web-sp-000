732bd5485a211f0c6f51fca81b8bb18850cf29b3
proactive-analyst-6157 <u.batsaikhan@gmail.com>
2020-04-18 19:18:59 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,9 +1,17 @@
 class School
-  def add_student=(student, grade)
-    @student = student
-    @grade = grade
+  attr_reader :school_name, :roster
+  
+  def initialize(school_name)
+    @school_name = school_name
+    @roster = {}
+  end
+  
+  def add_student(name, grade)
+    if @roster.keys.include? grade
+      @roster[grade] << name
+    else
+      @roster[grade] = []
+      @roster[grade] << name
   end
 end
 
-school = School.new("Bayside High School")
-school.roster
